0a593396607bbc98c94098c53366afbc3097f74d
sikonggpw <1327325804@qq.com>
2023-06-02 17:46:12 +0800

    fix:解决跨域问题

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+const origin = (res) => {
+  // 解决跨域问题
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+}
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+app.get('/', (req, res) => {
+  res.send('Home Page Route')
+  origin(res)
+});
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+app.get('/about', (req, res) => {
+  res.send('About Page Route')
+  origin(res)
+});
 
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/portfolio', (req, res) => {
+  res.send('Portfolio Page Route')
+  origin(res)
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact Page Route')
+  origin(res)
+});
 
 const port = process.env.PORT || 3000;
 
