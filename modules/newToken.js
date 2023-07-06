10c004050098eb90fab6da99bb11d713447c8f96
sikonggpw <1327325804@qq.com>
2023-07-06 14:06:59 +0800

    fix:token失效改为1天

--- modules/newToken.js
+++ modules/newToken.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: sikonggpw 1327325804@qq.com
+ * @Date: 2023-06-06 23:39:35
+ * @LastEditors: sikonggpw 1327325804@qq.com
+ * @LastEditTime: 2023-07-06 14:06:05
+ * @FilePath: \vercel-node-app\modules\newToken.js
+ * @Description:  生成新的token
+ */
 const jwt = require('express-jwt');
 const keyObj = require('../key');
 
 const newToken = (req, res) => {
-  const token = jwt.sign({ username }, keyObj.myKey, { expiresIn: '1h' });
+  const token = jwt.sign({ username }, keyObj.myKey, { expiresIn: '24h' });
   res.send({
     status: 200,
     token
