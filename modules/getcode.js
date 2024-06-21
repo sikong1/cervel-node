81485f9a840bbdf330786a7b7956e6fdd032427f
郭鹏伟 <guopengwei@xmbjkj.com>
2024-06-21 10:06:01 +0800

    docs: 测试提交

--- modules/getcode.js
+++ modules/getcode.js
@@ -7,6 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // const repData = require('../data/repData');
+
 const repDataClick = require('../data/repDataClick');
 const { aesDecrypt, generateRandomString, isMatch, isInterior, getClickData, successData, errorData } = require('../utils/crypot');
 const { LoginEnum } = require('../enum');
