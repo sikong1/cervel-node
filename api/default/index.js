396b7ec18c3431b41ffd49d6aac71339c3e1f152
郭鹏伟 <guopengwei@xmbjkj.com>
2024-09-11 16:36:15 +0800

    fix: 白名单路由

--- api/default/index.js
+++ api/default/index.js
@@ -7,32 +7,36 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = [
-    {
-        path: '/',
-        method: 'get',
-        handler: (req, res) => {
-            res.send('Home Page Route')
-        }
-    },
-    {
-        path: '/about',
-        method: 'get',
-        handler: (req, res) => {
-            res.send('about Page Route')
-        }
-    },
-    {
-        path: '/portfolio',
-        method: 'get',
-        handler: (req, res) => {
-            res.send('portfolio Page Route')
-        }
-    },
-    {
-        path: '/contact',
-        method: 'get',
-        handler: (req, res) => {
-            res.send('contact Page Route')
-        }
-    },
+  {
+    path: "/",
+    method: "get",
+    isWhiteList: true,
+    handler: (req, res) => {
+      res.send("Home Page Route")
+    }
+  },
+  {
+    path: "/about",
+    method: "get",
+    isWhiteList: true,
+    handler: (req, res) => {
+      res.send("about Page Route")
+    }
+  },
+  {
+    path: "/portfolio",
+    method: "get",
+    isWhiteList: true,
+    handler: (req, res) => {
+      res.send("portfolio Page Route")
+    }
+  },
+  {
+    path: "/contact",
+    method: "get",
+    isWhiteList: true,
+    handler: (req, res) => {
+      res.send("contact Page Route")
+    }
+  }
 ]
